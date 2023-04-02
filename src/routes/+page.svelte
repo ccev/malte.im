8c08d5f0c0ffc049b0670e8eb7ad53bb1b7f3c45
Malte <42342921+ccev@users.noreply.github.com>
2023-04-02 19:30:46 +0200

    smaller max width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
     export let data: ApiResponse
 </script>
 
-<div class="m-auto max-w-7xl grid gap-3 place-items-start min-[570px]:grid-cols-2 min-[860px]:grid-cols-3">
+<div class="m-auto max-w-5xl grid gap-3 place-items-start min-[570px]:grid-cols-2 min-[860px]:grid-cols-3">
     {#each data.characters as char}
         <GruntBox char={char}/>
     {/each}
