904405160a602f578b5ed3810d842519d012ebe8
Malte <42342921+ccev@users.noreply.github.com>
2025-03-19 17:33:58 +0100

    deprecation notice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,32 @@
     import Discord from "../components/icons/Discord.svelte";
     import {_} from "svelte-i18n";
     import KofiIcon from "../components/icons/KofiIcon.svelte";
+    import Box from "../components/design/Box.svelte";
 </script>
 
 <Layout scrollToFooter={true}>
     <div class="max-w-5xl mx-auto grid gap-4">
+        <Box
+            class="w-full bg-back text-fore text-base p-4"
+            rounded
+        >
+            <p class="mb-4">
+                Hi!
+            </p>
+            <p class="mb-4">
+                Unfortunately, I've lost interest in the game and stopped maintaining malte.im.
+                This site will be kept up for archival purposes.
+                <b class="font-semibold">All data is outdated and not accurate!</b>
+            </p>
+            <p>
+                Luckily, a friend is currently working on a replacement.
+                <a class="text-sky-500 underline visited:text-purple-600" href="https://discord.gg/akNbA6v8Ug">
+                    Join the malte.im Discord
+                </a>
+                to know when it's finished and where to find it!
+            </p>
+        </Box>
+
         <HeroRocket/>
         <HeroRaids/>
 
